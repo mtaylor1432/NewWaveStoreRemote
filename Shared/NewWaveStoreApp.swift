530cce59d2d2e35f0cbb10b3ca6c70c551e82509
Maya Taylor <s016719@students.lmsd.org>
2022-11-21 13:58:44 -0500

    Initial Commit

---
+++ Shared/NewWaveStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewWaveStoreApp.swift
+//  Shared
+//
+//  Created by Maya Taylor (student LM) on 11/21/22.
+//
+
+import SwiftUI
+
+@main
+struct NewWaveStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
